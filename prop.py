b6396f20fe7e80c45b31fcecc3e05445d27fbfa7
Dana5371 <danaker.qw@gmail.com>
2021-12-26 21:56:04 +0600

    fourth commit

--- prop.py
+++ prop.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup as BS
 import csv
 
 
-def get_html(url):
-    responce = requests.get(url)
-    return responce.text
+def get_html_third(url_osh):
+    re = requests.get(url_osh)
+    return re.text
 
 
 def get_html_doska(url_doska):
@@ -13,6 +13,72 @@ def get_html_doska(url_doska):
     return r.text
 
 
+def get_html(url):
+    responce = requests.get(url)
+    return responce.text
+
+
+def get_third_site(osh):
+    third = BS(osh, 'html5lib')
+    lists = third.find('div', class_='listings-wrapper')
+    aparts_osh = lists.find_all('div', class_='listing')
+    for apart in aparts_osh:
+        try:
+           title = apart.find('p', class_='title').text.strip()
+           location = apart.find('div', class_='address').text.strip()
+           price_dollar = apart.find('div', class_='price').text.strip()
+           price_som = apart.find('div', class_='price-addition').text.strip()
+           description = apart.find('div', class_='description').text.strip()
+           link = apart.find('a').get('href')
+       
+        except:
+            title = ''
+            location = ''
+            price_dollar = ''
+            price_som = ''
+            description = ''
+            link = ''
+
+        data = {
+            'title': title,
+            'location': location,
+            'price': f'{price_dollar},{price_som}',
+            'description': description,
+            'link': f'https://www.house.kg{link}'
+        }   
+ 
+        write_to_csv(data)
+        
+
+
+
+
+
+def get_second_site(doska):
+    beauty = BS(doska, 'html5lib')
+    listt = beauty.find('div', class_='doska_last_items_list')
+    apartss = listt.find_all('div', class_='list_full dev')
+    for ap in apartss:
+        try:
+            title = ap.find('a', class_='title_url').text.strip()
+            price = ap.find('div', class_='list_full_price').text.strip()
+            link = ap.find('a', class_='title_url').get('href')
+           
+        except:
+            title = ''
+            price = ''
+            link = ''
+           
+        
+        data = {
+            'title': title,
+            'price': price,
+            'link': f'https://doska.kg{link}',
+            'location' : 'Not found',
+            'description': 'Not found'
+        }   
+ 
+        write_to_csv(data)
 
 def get_data(html):
     soup = BS(html, 'html5lib')
@@ -43,34 +109,6 @@ def get_data(html):
             'price': f'{price_dollar},{price_som}',
             'description': description,
             'link': f'https://www.house.kg{link}'
-        }   
- 
-        write_to_csv(data)
-
-
-def get_second_site(doska):
-    beauty = BS(doska, 'html5lib')
-    listt = beauty.find('div', class_='doska_last_items_list')
-    apartss = listt.find_all('div', class_='list_full dev')
-    for ap in apartss:
-        try:
-            title = ap.find('a', class_='title_url').text.strip()
-            price = ap.find('div', class_='list_full_price').text.strip()
-            link = ap.find('a', class_='title_url').get('href')
-           
-        except:
-            title = ''
-            price = ''
-            link = ''
-            location = ''
-            description = ''
-        
-        data = {
-            'title': title,
-            'price': price,
-            'link': f'https://doska.kg{link}',
-            'location' : 'Not found',
-            'description': 'Not found'
         }   
  
         write_to_csv(data)
@@ -107,6 +145,17 @@ def run_doska():
         get_second_site(html_second)
         i += 1
 
+def run_osh():
+    i = 1
+    while True:
+        url_osh = 'https://www.house.kg/kupit?region=6&town=36&page={i}'
+        html_third = get_html_third(url_osh)
+        if i == 20:
+            break
+        get_third_site(html_third)
+        i += 1
+
+
 def main():
     with open('apart.csv', 'w') as file:pass
     write_headers()
@@ -119,6 +168,7 @@ def main():
         get_data(html=html)
         i += 1
     run_doska()
+    run_osh()
    
 
 
